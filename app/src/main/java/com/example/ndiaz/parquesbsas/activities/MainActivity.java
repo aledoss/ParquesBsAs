793125e846cb90376b2edb81cb89946d8c435e90
Nicolas <len_we@hotmail.com>
2016-10-12 11:26:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ndiaz/parquesbsas/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ndiaz.parquesbsas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/ndiaz/parquesbsas/activities/MainHome.java
@@ -0,0 +1,8 @@
+package com.example.ndiaz.parquesbsas.activities;
+
+/**
+ * Created by ndiaz on 12/10/2016.
+ */
+
+public class MainHome {
+}
