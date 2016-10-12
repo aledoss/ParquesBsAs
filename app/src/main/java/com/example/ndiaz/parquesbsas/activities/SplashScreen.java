c07d04668ddc5f4424e406280234b3b8527aa967
Nicolas <len_we@hotmail.com>
2016-10-12 17:48:07 -0300

    Mejoras estéticas

--- app/src/main/java/com/example/ndiaz/parquesbsas/activities/SplashScreen.java
+++ app/src/main/java/com/example/ndiaz/parquesbsas/activities/SplashScreen.java
@@ -14,10 +14,9 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
 
         Thread thread = new Thread() {
             @Override
