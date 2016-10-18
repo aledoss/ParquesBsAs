4334da27c9365f04dd23e58c313b907b7bc2100f
Nicolas <len_we@hotmail.com>
2016-10-18 00:55:22 -0300

    Navigation Drawer Maps

--- app/src/main/java/com/example/ndiaz/parquesbsas/activities/MainHome.java
+++ app/src/main/java/com/example/ndiaz/parquesbsas/activities/MainHome.java
@@ -1,22 +1,33 @@
 package com.example.ndiaz.parquesbsas.activities;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ndiaz.parquesbsas.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainHome extends AppCompatActivity {
+public class MainHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toogle;
+    private NavigationView navigationView;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +40,14 @@ public class MainHome extends AppCompatActivity {
     private void setupUI() {
         setupToolbar();
         setupDrawerLayout();
+        setupMap();
+    }
 
-
+    private void setupToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
     }
 
     private void setupDrawerLayout() {
@@ -52,6 +69,14 @@ public class MainHome extends AppCompatActivity {
         };
         drawerLayout.addDrawerListener(toogle);
         toogle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setupMap() {
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa_home);
+        supportMapFragment.getMapAsync(this);
     }
 
     @Override
@@ -66,11 +91,6 @@ public class MainHome extends AppCompatActivity {
         toogle.onConfigurationChanged(newConfig);
     }
 
-    private void setupToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar_home);
-        setSupportActionBar(toolbar);
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (toogle.onOptionsItemSelected(item)) {
@@ -78,5 +98,48 @@ public class MainHome extends AppCompatActivity {
         }
         //int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_menu1:
+                Toast.makeText(this, "caballito", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_menu2:
+                Toast.makeText(this, "cat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_menu_logout:
+                finish();
+                break;
+            case R.id.nav_menu_settings:
+                startActivity(new Intent(MainHome.this, MySettings.class));
+                break;
+        }
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap = googleMap;
+        LatLng capitalFederal = new LatLng(-34.612892,-58.4707548);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(capitalFederal,11));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/example/ndiaz/parquesbsas/activities/MySettings.java
@@ -0,0 +1,15 @@
+package com.example.ndiaz.parquesbsas.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ndiaz.parquesbsas.R;
+
+public class MySettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_settings);
+    }
+}
