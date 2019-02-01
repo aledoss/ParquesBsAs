971c03cd9870028dbad4d4d9df1558d81fca15d5
Lenwe <len_we@hotmail.com>
2019-01-31 23:04:55 -0300

    Captura codigo error servicio

--- app/src/main/java/com/example/ndiaz/parquesbsas/network/RetrofitService.java
+++ app/src/main/java/com/example/ndiaz/parquesbsas/network/RetrofitService.java
@@ -3,7 +3,12 @@ package com.example.ndiaz.parquesbsas.network;
 import com.example.ndiaz.parquesbsas.BuildConfig;
 import com.example.ndiaz.parquesbsas.helpers.JSONConvert;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -32,8 +37,34 @@ public class RetrofitService {
     }
 
     private OkHttpClient getOkHttpClient() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        return new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(getInterceptor());
+        builder.addNetworkInterceptor(createLoggingInterceptor());
+
+        return builder.build();
+    }
+
+    private Interceptor getInterceptor() {
+        return chain -> {
+            Request request = chain.request();
+            Response response = chain.proceed(request);
+            if (response.code() < 200 || response.code() >= 300) {
+                throw new IOException("Error de conexión con el servidor. Intente más tarde.");
+            }
+
+            return response;
+        };
+    }
+
+    private HttpLoggingInterceptor createLoggingInterceptor() {
+        HttpLoggingInterceptor loggingInterceptor;
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        } else {
+            loggingInterceptor = new HttpLoggingInterceptor(message -> {
+            });
+        }
+        return loggingInterceptor;
     }
 }
